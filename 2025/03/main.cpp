d5ec6d6b99587506ad040f98036e86352d33e6d6
Joao Carreira <jfmcarreira@gmail.com>
2025-12-04 17:03:46 +0000

    feat(2025): day 3 part two

--- 2025/03/main.cpp
+++ 2025/03/main.cpp
@@ -8,13 +8,14 @@
 #include <vector>
 
 constexpr std::string_view kExampleInput{"987654321111111\n811111111111119\n234234234234278\n818181911112111"};
+constexpr std::int64_t kDigitsCount{12};
 
-constexpr auto to_number(std::span<const std::int64_t> digits) noexcept
+constexpr auto to_number(std::span<const std::int64_t> digits, std::span<const std::int64_t> indexes) noexcept
 {
     std::int64_t number{0};
     std::int64_t base{1};
-    for (auto digit : digits | std::views::reverse) {
-        number += digit * base;
+    for (auto index : indexes | std::views::reverse) {
+        number += digits[index] * base;
         base *= 10;
     }
     return number;
@@ -25,19 +26,44 @@ constexpr auto to_number(std::int64_t dozens, std::int64_t units) noexcept -> st
     return dozens * 10 + units;
 }
 
+constexpr auto check_max(
+    std::span<const std::int64_t> digits,
+    std::span<std::int64_t> best_digits,
+    std::int64_t opt_index) noexcept -> std::int64_t
+{
+    auto digits_count = std::ssize(digits);
+
+    if (opt_index >= kDigitsCount) {
+        return to_number(digits, best_digits);
+    }
+
+    best_digits[opt_index] = opt_index > 0 ? best_digits[opt_index - 1] + 1 : 0;
+    auto end = digits_count - (kDigitsCount - opt_index) + 1;
+
+    std::int64_t max_digit{0};
+    std::int64_t max_value{0};
+    while (best_digits[opt_index] < end) {
+        if (digits[best_digits[opt_index]] > max_digit) {
+            max_digit = digits[best_digits[opt_index]];
+            auto max = check_max(digits, best_digits, opt_index + 1);
+            if (max > max_value) {
+                max_value = max;
+            }
+        }
+        ++best_digits[opt_index];
+    }
+    return max_value;
+}
+
 constexpr auto parse_line(std::string_view input_line) noexcept -> std::int64_t
 {
     auto to_numbers = std::views::transform([](const auto& c) { return static_cast<std::int64_t>(c) - 48; });
-    auto digits = input_line | to_numbers;
+    auto digits = input_line | to_numbers | std::ranges::to<std::vector>();
 
-    std::int64_t max_value{0};
-    auto it = digits.begin();
-    while (it != digits.end()) {
-        auto value = *std::ranges::max_element(
-            it + 1, digits.end(), std::less<>{}, [&](auto value) { return to_number(*it, value); });
-        max_value = std::max(to_number(*it, value), max_value);
-        ++it;
-    }
+    std::array<std::int64_t, kDigitsCount> best_digits;
+    std::ranges::copy(std::views::iota(0) | std::views::take(kDigitsCount), best_digits.begin());
+
+    auto max_value = check_max(digits, best_digits, 0);
 
     // for (auto number : digits) {
     //     std::cout << number << " ";
@@ -68,7 +94,7 @@ int main(int argc, char* argv[])
     }
     std::string filename{argv[1]};
 
-    static_assert(solve(kExampleInput) == 357);
+    //  static_assert(solve(kExampleInput) == 357);
 
     if (filename == "inline_example") {
         auto result = solve(kExampleInput);
@@ -83,4 +109,4 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-// 17263
+// 170731717900423
