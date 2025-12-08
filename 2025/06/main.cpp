6f079774b770b0449af3137d6f90a7ccef558b0d
Joao Carreira <jfmcarreira@gmail.com>
2025-12-08 04:12:23 +0000

    feat(2025): clean up solution for day 6

--- 2025/06/main.cpp
+++ 2025/06/main.cpp
@@ -8,19 +8,19 @@
 
 constexpr std::string_view kExampleInput{"123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  "};
 
-template<typename T>
 struct Operation
 {
-    std::vector<T> inputs;
+    std::vector<std::string> inputs;
     char op;
 };
 
-constexpr auto compute_operation(const Operation<std::int64_t>& operation) noexcept
+constexpr auto compute_operation(const Operation& operation) noexcept
 {
+    auto inputs = operation.inputs | std::views::transform([](const auto& text) { return from_chars(text); });
     if (operation.op == '*') {
-        return std::ranges::fold_left(operation.inputs, 1, std::multiplies<>{});
+        return std::ranges::fold_left(inputs, 1, std::multiplies<>{});
     }
-    return std::ranges::fold_left(operation.inputs, 0, std::plus<>{});
+    return std::ranges::fold_left(inputs, 0, std::plus<>{});
 }
 
 constexpr auto solve(std::string_view input) noexcept -> std::int64_t
@@ -29,21 +29,13 @@ constexpr auto solve(std::string_view input) noexcept -> std::int64_t
     auto width = std::ranges::max_element(matrix, std::less<>{}, [](const auto& line) { return line.size(); })->size();
 
     for (std::int64_t i = 0; i < width; ++i) {
-        bool is_col{true};
-        for (std::int64_t j = 0; j < matrix.size(); ++j) {
-            if (matrix[j][i] != ' ') {
-                is_col = false;
-                break;
-            }
-        }
-        if (is_col) {
-            for (std::int64_t j = 0; j < matrix.size(); ++j) {
-                matrix[j][i] = ',';
-            }
-        }
+        auto column =
+            std::views::iota(0U, matrix.size()) | std::views::transform([&](auto y) -> auto& { return matrix[y][i]; });
+        if (!std::ranges::all_of(column, [](auto c) { return c == ' '; })) continue;
+        std::ranges::fill(column, ',');
     }
 
-    std::vector<Operation<std::string_view>> operations;
+    std::vector<Operation> operations;
     for (const auto& matrix_line : matrix) {
         std::string_view line{matrix_line};
         std::int64_t col_idx{0};
@@ -53,13 +45,11 @@ constexpr auto solve(std::string_view input) noexcept -> std::int64_t
             if (number[0] == '+' || number[0] == '*') {
                 operations[col_idx].op = line[0];
             } else {
+                auto input = number | std::views::reverse | std::ranges::to<std::string>();
                 if (operations.size() <= col_idx) {
-                    operations.push_back(
-                        Operation<std::string_view>{
-                            .inputs = {number},
-                        });
+                    operations.push_back(Operation{.inputs = {std::move(input)}});
                 } else {
-                    operations[col_idx].inputs.push_back(number);
+                    operations[col_idx].inputs.push_back(std::move(input));
                 }
             }
             if (line_break_pos == std::string_view::npos) break;
@@ -67,26 +57,26 @@ constexpr auto solve(std::string_view input) noexcept -> std::int64_t
             ++col_idx;
         }
     }
-    std::vector<Operation<std::int64_t>> right_to_left_operations;
+
+    std::vector<Operation> right_to_left_operations;
     right_to_left_operations.reserve(operations.size());
 
     for (const auto& operation : operations) {
-
-        Operation<std::int64_t> right_to_left_operation;
-        right_to_left_operation.op = operation.op;
-
         auto input_length = operation.inputs[0].size();
-
-        for (std::int64_t i = input_length - 1; i >= 0; --i) {
+        std::vector<std::string> inputs;
+        for (std::int64_t i = 0; i < input_length; ++i) {
             std::string number_chars;
             for (std::int64_t j = 0; j < operation.inputs.size(); ++j) {
                 auto character = operation.inputs[j][i];
                 if (character == ' ') continue;
                 number_chars.push_back(character);
             }
-            right_to_left_operation.inputs.push_back(from_chars(number_chars));
+            inputs.push_back(std::move(number_chars));
         }
-        right_to_left_operations.push_back(std::move(right_to_left_operation));
+        right_to_left_operations.push_back({
+            .inputs = std::move(inputs),
+            .op = operation.op,
+        });
     }
     return std::ranges::fold_left(
         right_to_left_operations | std::views::transform(compute_operation), 0, std::plus<>{});
